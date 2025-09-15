189bb11b62cbf2191aab070fcc5936a4723a31a6
Sewer56 <sewer56lol@googlemail.com>
2025-09-15 03:05:20 +0100

    added: flake for building the package

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "A plugin to use different layouts on workspaces";
+
+  inputs = {
+    nixpkgs.follows = "hyprland/nixpkgs";
+    systems.follows = "hyprland/systems";
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    systems = ["x86_64-linux"];
+    eachSystem = nixpkgs.lib.genAttrs systems;
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprWorkspaceLayouts;
+      hyprWorkspaceLayouts = pkgsFor.${system}.callPackage ({
+        lib,
+        hyprlandPlugins,
+      }:
+        hyprlandPlugins.mkHyprlandPlugin inputs.hyprland.packages.${system}.hyprland {
+          pluginName = "hyprWorkspaceLayouts";
+          version = "0-unstable-2025-09-15";
+
+          src = ./.;
+
+          nativeBuildInputs = [pkgsFor.${system}.gnumake];
+
+          buildPhase = ''
+            runHook preBuild
+            make all
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/lib
+            mv workspaceLayoutPlugin.so $out/lib/libhyprWorkspaceLayouts.so
+            runHook postInstall
+          '';
+
+          meta = {
+            homepage = "https://github.com/zakk4223/hyprWorkspaceLayouts";
+            description = "Workspace-specific window layouts for Hyprland";
+            license = lib.licenses.bsd3;
+            platforms = lib.platforms.linux;
+            maintainers = [];
+          };
+        }) {};
+    });
+
+    devShells = eachSystem (system: {
+      default = pkgsFor.${system}.mkShell.override {stdenv = pkgsFor.${system}.gcc14Stdenv;} {
+        name = "hyprWorkspaceLayouts";
+        buildInputs = [inputs.hyprland.packages.${system}.hyprland];
+        inputsFrom = [inputs.hyprland.packages.${system}.hyprland];
+      };
+    });
+  };
+}
